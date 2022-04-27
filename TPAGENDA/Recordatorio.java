cdec3cf6d9458a46a6ea0710b1b9178f31fbe64e
Jere <mjeremiasadriel@gmail.com>
2022-04-27 20:50:14 -0300

    constructor de recordatorio

--- TPAGENDA/Recordatorio.java
+++ TPAGENDA/Recordatorio.java
@@ -1,11 +1,14 @@
 package TPAGENDA;
 
 public class Recordatorio extends Agenda {
-
+    
     //Defini una variable del tipo string 
     protected String hora;
     
-    
+    public Recordatorio(String Nombre,String Descripcion,String Fecha){
+        super(Nombre, Descripcion, Fecha);
+    }
+
     public void set_Hora(String hora){
         this.hora = hora;    
     }
@@ -13,6 +16,8 @@ public class Recordatorio extends Agenda {
     public String get_Hora(){
         return hora;
     }
+
+
 
 }
 
