e4705f52dfecc2140b4fb4694ff92840be9c0bca
Fernando Grabobi <85689008+FernandoGrabobi@users.noreply.github.com>
2022-04-29 13:07:59 -0300

    46

--- TPAGENDA/App.java
+++ TPAGENDA/App.java
@@ -1,33 +1,50 @@
 package TPAGENDA;
 import java.util.Scanner;
-///import java.text.SimpleDateFormat;
-///import java.util.Date;
+import java.util.ArrayList;
+
 
 public class App {
-
-    ///public static SimpleDateFormat fecha = new SimpleDateFormat("dd/MM/YYYY"); 
     public static void main(String[] args) {
     
- 
-        
-        ///Date fechaActual = new Date();       
         Scanner input = new Scanner(System.in);
 
+    
         int menu = 9;
- 
+        int op1 = 0;
+        ArrayList<Evento> events = new ArrayList<Evento>();
+        ArrayList<Tarea> tasks = new ArrayList<Tarea>();
+        ArrayList<Recordatorio> remiders = new ArrayList<Recordatorio>();
+
         while(menu != 0){
-            System.out.println(" Presione 1 para cargar una nueva Tarea-Evento-Recordatorio a tu agenda ");
-            System.out.println(" Presione 2 para buscar una Tarea-Evento-Recordatorio de tu agenda y modificarla");
-            System.out.println(" Presione 3 para eliminar una Tarea-Evento-Recordatorio de tu agenda");
-            System.out.println(" Presione 0 para salir del programa");
+            System.out.println("\n Presione 1 para cargar una nueva Tarea-Evento-Recordatorio a tu agenda ");
+            System.out.println("\n Presione 2 para buscar una Tarea-Evento-Recordatorio de tu agenda y modificarla ");
+            System.out.println("\n Presione 3 para eliminar una Tarea-Evento-Recordatorio de tu agenda ");
+            System.out.println("\n Presione 0 para salir del programa ");
             menu = input.nextInt();
             input.nextLine();
 
             switch(menu){
                 case 1:
                 System.out.print("\033[H\033[2J");                
+                System.out.println("\n Que desea ingresar: \n 1 para ingresar una tarea \n 2 para ingresar un evento \n 3 para ingresar un recordatorio\n");
+                op1 = input.nextInt();
+                input.nextLine();
 
+                switch(op1){
+                    case 1:
 
+                    break;
+                    case 2:
+
+                    break;
+                    case 3:
+
+                    break;
+                    default:
+                    System.out.print("\033[H\033[2J");
+                    System.out.println("\n Ingrese una opcion valida ");
+                    break;
+                }
                 
                 break;
                 case 2:
@@ -47,7 +64,7 @@ public class App {
                 break;
                 default:
                 System.out.print("\033[H\033[2J");
-                System.out.println(" Ingrese una opcion valida ");
+                System.out.println(" \n Ingrese una opcion valida ");
                 break;
 
             }
@@ -58,11 +75,5 @@ public class App {
 
         input.close();
     }
-
-
-
-
-
-
 
 }
